e84f7ad0359281d2d556bd0c6379c47c488cf947
Fletacode <88104352+Fletacode@users.noreply.github.com>
2023-09-16 14:08:34 +0000

    관사 검색 라우터 끝

--- routers/boq.js
+++ routers/boq.js
@@ -16,13 +16,46 @@ router.get('/info/:st', async (req,res)=>{
     }
 })
 
-router.get('/search', async (req,res)=>{
-    console.log(req.query);
+router.get('/search/:st', async (req,res)=>{
+   
+    
+    let searchQuery = [
+            {
+            $search: {index: 'default', 
+                      text: {
+                          query: req.query.input, 
+                          path: ['mng_unit','build_nm'] 
+                      },
+                    },
+            },
+            {
+                $match:{
+                    sido: req.query.sido,
+                    a_dvs:req.query.armyType
+                }
+            },
+            {
+             $skip: parseInt(req.params.st), // skip 스테이지 추가
+            },
+            {
+                $limit: 10, // limit 스테이지 추가
+            },
+        ];
+    
+        if (!req.query.input) searchQuery.shift();
 
     try{
-        const boqInfos = await  OutsideBoq.find({})
-    }catch{
-
+        
+        let boqInfos = await  OutsideBoq.aggregate(searchQuery).exec();
+        const boqLength = await OutsideBoq.countDocuments(searchQuery);
+        if (!boqInfos) {
+            searchQuery.shift();
+            boqInfos = await OutsideBoq.aggregate(searchQuery).exec();
+        }
+        return res.json({isSuccess:true,boqs:boqInfos,length:boqLength});
+    }catch (err){
+        console.log(err);
+        return res.json({isSuccess:false});
     }
 })
 
